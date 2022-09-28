e10c8721350acde3b5bc763919870ee60bb1508b
Oskar Mikolajczyk <omikolaj1@gmail.com>
2022-09-28 18:06:01 -0400

    bug: fix code

--- Leadsly.Domain/MQ/Services/AllInOneVirtualAssistantMQService.cs
+++ Leadsly.Domain/MQ/Services/AllInOneVirtualAssistantMQService.cs
@@ -1,8 +1,11 @@
 ﻿using Leadsly.Domain.Decorators;
+using Leadsly.Domain.Models;
+using Leadsly.Domain.Models.Entities;
 using Leadsly.Domain.MQ.Messages;
 using Leadsly.Domain.MQ.Services.Interfaces;
 using Leadsly.Domain.Services.Interfaces;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -30,51 +33,51 @@ namespace Leadsly.Domain.MQ.Services
         public async Task<bool> ProvisionResourcesAsync(string halId, string userId, CancellationToken ct = default)
         {
             // save it to the database
-            //VirtualAssistant virtualAssistant = await _virtualAssistantRepository.GetByHalIdAsync(halId, ct);
-            //if (virtualAssistant == null)
-            //{
-            //    _logger.LogError("Virtual assistant is not found by HalId {0} for UserId {1}", halId, userId);
-            //    return false;
-            //}
+            VirtualAssistant virtualAssistant = await _virtualAssistantRepository.GetByHalIdAsync(halId, ct);
+            if (virtualAssistant == null)
+            {
+                _logger.LogError("Virtual assistant is not found by HalId {0} for UserId {1}", halId, userId);
+                return false;
+            }
 
-            //// 1. create new hal ecs service
-            //EcsService halEcsService = virtualAssistant.EcsServices.FirstOrDefault(s => s.Purpose == EcsResourcePurpose.Hal);
-            //if (halEcsService == null)
-            //{
-            //    _logger.LogError("Expected to find previous Hal ecs service but none was found off of virtual assistant");
-            //    return false;
-            //}
+            // 1. create new hal ecs service
+            EcsService halEcsService = virtualAssistant.EcsServices.FirstOrDefault(s => s.Purpose == EcsResourcePurpose.Hal);
+            if (halEcsService == null)
+            {
+                _logger.LogError("Expected to find previous Hal ecs service but none was found off of virtual assistant");
+                return false;
+            }
 
-            //if (await _service.CreateAwsEcsServiceAsync(userId, halEcsService, ct) == false)
-            //{
-            //    _logger.LogError("Failed to create Hal ecs service in aws");
-            //    return false;
-            //}
+            if (await _service.CreateAwsEcsServiceAsync(userId, halEcsService, ct) == false)
+            {
+                _logger.LogError("Failed to create Hal ecs service in aws");
+                return false;
+            }
 
-            //// 2. create new grid ecs service
-            //EcsService gridEcsService = virtualAssistant.EcsServices.FirstOrDefault(s => s.Purpose == EcsResourcePurpose.Grid);
-            //if (gridEcsService == null)
-            //{
-            //    _logger.LogError("Expected to find previous Grid ecs service but none was found off of virtual assistant");
-            //    return false;
-            //}
+            // 2. create new grid ecs service
+            EcsService gridEcsService = virtualAssistant.EcsServices.FirstOrDefault(s => s.Purpose == EcsResourcePurpose.Grid);
+            if (gridEcsService == null)
+            {
+                _logger.LogError("Expected to find previous Grid ecs service but none was found off of virtual assistant");
+                return false;
+            }
 
-            //if (await _service.CreateAwsEcsServiceAsync(userId, halEcsService, ct) == false)
-            //{
-            //    _logger.LogError("Failed to create Grid ecs service in aws");
-            //    return false;
-            //}
+            if (await _service.CreateAwsEcsServiceAsync(userId, halEcsService, ct) == false)
+            {
+                _logger.LogError("Failed to create Grid ecs service in aws");
+                return false;
+            }
 
-            //// virtualAssistant.EcsServices = _service.EcsServices;
-            //virtualAssistant.Provisioned = true;
-            //virtualAssistant = await _virtualAssistantRepository.UpdateAsync(virtualAssistant, ct);
-            //if (virtualAssistant == null)
-            //{
-            //    _logger.LogError("Failed to successfully update virtual assistant's cloud resources.");
-            //    // here we need to roll back all of the resources
-            //    await _service.RollbackAllResourcesAsync(userId, ct);
-            //    return false;
-            //}
+            // virtualAssistant.EcsServices = _service.EcsServices;
+            virtualAssistant.Provisioned = true;
+            virtualAssistant = await _virtualAssistantRepository.UpdateAsync(virtualAssistant, ct);
+            if (virtualAssistant == null)
+            {
+                _logger.LogError("Failed to successfully update virtual assistant's cloud resources.");
+                // here we need to roll back all of the resources
+                await _service.RollbackAllResourcesAsync(userId, ct);
+                return false;
+            }
 
             return true;
 
